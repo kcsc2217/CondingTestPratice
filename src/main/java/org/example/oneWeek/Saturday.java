0c5f538215e52ccca12ff75068796b256da42016
kcsc2217 <k12002@nate.com>
2024-06-15 15:14:22 +0900

    1주차 토요일 과제

---
+++ src/main/java/org/example/oneWeek/Saturday.java
@@ -0,0 +1,35 @@
+package org.example.oneWeek;
+
+import java.util.Arrays;
+
+public class Saturday {
+    public long solution(int n, int[] times) {
+        long answer = 0;
+
+        Arrays.sort(times);
+
+        long left = 0;
+        long right= times[times.length-1] * (long)n; // 주어진 n이 입국심사를 가장 늦게 받을 시간
+
+        while(left <= right){
+            long mid = (left + right) / 2;
+            long result = 0;
+
+
+            for(int time : times){
+                result += mid / time;
+            }
+
+            if(result < n){
+                left  = mid+1;
+            }
+            else{
+                answer = mid;
+                right = mid-1;
+            }
+        }
+
+
+        return answer;
+    }
+}
