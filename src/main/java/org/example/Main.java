d98894aaca72cd00c127ba82eaf8ca605b901cbb
kcsc2217 <k12002@nate.com>
2024-09-13 00:15:19 +0900

    부분수열의 합 - 백준

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,141 +3,59 @@ package org.example;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
 
-    static int arr[][];
-    static boolean visit[][];
-    static StringBuilder sb;
+    static int N , S;
 
-    static int dx[] ={-1, 1, 0, 0};
-    static int dy[] = {0, 0, -1, 1};
+    static int count = 0;  // count 와 sum 초기화
 
-    // 이동 좌표 상하 좌우
-
-    static int aw = 0;
-
-
-    static int N;
+    static int arr[];
 
 
     public static void main(String[] args) throws IOException {
 
-         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-          sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-          N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-          arr = new int[N][N];
-          visit = new boolean[N][N];
+        N = Integer.parseInt(st.nextToken());   //N 과 S 초기화
+        S = Integer.parseInt(st.nextToken());
 
+        arr =new int[N]; //배열 초기화
 
-          for(int i = 0; i < N; i++){
-              String str = br.readLine();
-              for(int j = 0; j < N; j++){
-                  arr[i][j] = str.charAt(j) - '0';
-              }
-          }
-          List<Integer> list = new ArrayList<>();
+        st = new StringTokenizer(br.readLine());
+
+        for(int i =0; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }  // arr배열 초기화
 
 
-          for(int i = 0; i < N; i++ ){
-              for(int j = 0; j < N; j++){
-                  if(!visit[i][j] && arr[i][j] == 1){
-                      bfs(i, j);
-                      list.add(aw);
-                      aw = 0;
-                  }
-              }
-          }
+        dfs(0,0);
 
-          Collections.sort(list);
-
-        System.out.println(list.size());
-
-        for(int i : list){
-            System.out.println(i);
-        }
+        System.out.println(S == 0 ? count -1 : count);
 
     }
 
-         public static void dfs(int i, int j){
-            visit[i][j] = true;
-            aw++;
-            //방문하면 쌓기
+    static void dfs(int i,  int sum){
 
 
-            for(int k = 0;  k < 4; k++){
-                int next_x = i + dx[k];
-                int next_y = j + dy[k];
-
-                if(next_x >=0 && next_x < N && next_y >=0 && next_y < N){ // 즉 네모 안의 범위에 속하면
-                    if(!visit[next_x][next_y] && arr[next_x][next_y] == 1){
-                        dfs(next_x, next_y);
-                    }
-                }
+        if(i == N){
+            if(sum == S){
+                count++;
             }
-
-         }
-
-    public static void bfs(int i, int j){
-        Queue<Node> queue = new LinkedList<>();
-
-        visit[i][j] = true;
-        aw++;
-
-        queue.add(new Node(i,j));
-
-
-        while(!queue.isEmpty()){
-            Node node = queue.poll();
-            int x = node.getX();
-            int y = node.getY();
-
-
-            for(int k = 0; k < 4; k++){
-             int next_x = x + dx[k];
-             int next_y = y + dy[k];
-
-                if(next_x >=0 && next_x < N && next_y >=0 && next_y < N){ // 즉 네모 안의 범위에 속하면
-                    if(!visit[next_x][next_y] && arr[next_x][next_y] == 1){  //방문했다의 의미: 큐에 있다 !!
-                       visit[next_x][next_y] = true;
-                       aw++;
-                       queue.add(new Node(next_x, next_y));
-                    }
-                }
-            }
+            return;
         }
 
+        dfs(i+1, arr[i] + sum);  //선택
+        dfs(i+1, sum);  // 선택 x    즉 sum은 축적 값
 
     }
-
-
-
-    }
-    class Node{ // 큐에 x, y 좌표 둘다 관리하기 위해 넣어줬다.
-    int x;
-    int y;
-
-        public Node(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        public int getX() {
-            return x;
-        }
-
-        public int getY() {
-            return y;
-        }
-    }
-
-
+}
 
 
 
--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,45 +1,218 @@
 package org.example;
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
 
-class Solution {
-    public int[] solution(int[] prices) {
-        int n = prices.length;
-        int[] answer = new int[n];
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        // 테스트 케이스의 수 T 입력
+        int testCase = Integer.parseInt(br.readLine());
+
+        // 각 테스트 케이스에 대한 처리
+        for (int t = 1; t <= testCase; t++) {
+            sb.append("#").append(t).append(" ");
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int H = Integer.parseInt(st.nextToken()); //높이
+            int W = Integer.parseInt(st.nextToken()); // 넓이
+
+            char[][] arr = new char[H][W]; // 초기화 할 배열
+
+            for (int i = 0; i < H; i++) {
+                String str = br.readLine();
+
+                for (int j = 0; j < W; j++) {
+                    arr[i][j] = str.charAt(j);
+                }
+            } // 배열 초기화
+
+            int N = Integer.parseInt(br.readLine()); // 칸의 수
 
 
-        Queue<Integer> que = new LinkedList<>(); //
+            String moveStr = br.readLine(); // 컨트롤
 
-        for(int i : prices){
-            que.add(i);
-        }
+            battle(H, W, N, arr, moveStr);
 
-
-        int index = 0;
-
-        while(!que.isEmpty()){
-            int aw = que.poll();
-
-            for(int i = n - que.size(); i < n; i++){
-
-                if(prices[i] < aw){ //주식 가격이 떨어짐
-                    answer[index]++;
-                    break;
+            for(int j = 0; j < H; j++){
+                for(int k =0; k < W; k++) {
+                    System.out.print(arr[j][k]);
+                }
+                System.out.println();
                 }
 
-                if(aw <= prices[i]){
-                    answer[index]++;
+
+            // 결과 출력
+
+
+        }
+    }
+
+    public static void battle(int H, int W, int N, char[][] arr, String str){
+        for(int i = 0; i < N; i++){
+            char aw = str.charAt(i);
+
+            if(aw == 'S'){  //슈팅 쏠때
+                for(int j = 0; j < H; j++){
+                    for(int k =0; k < W; k++){
+                        if(arr[j][k] == '<'){  //좌측일떄
+
+                            for(int s = k-1; s >=0; s--){
+                                if(arr[j][s] == '*'){
+                                    arr[j][s] = '.';
+                                    break;
+                                }
+
+                                if(arr[j][s] == '#'){
+                                    break;
+                                }
+                            }
+                        }
+
+                        else if(arr[j][k] == '>'){  //좌측일떄
+
+                            for(int s = k+1; s < W; s++){
+                                if(arr[j][s] == '*'){
+                                    arr[j][s] = '.';
+                                    break;
+                                }
+
+                                if(arr[j][s] == '#'){
+                                    break;
+                                }
+                            }
+                        }
+
+                        else if(arr[j][k] == 'v'){  //좌측일떄
+
+                            for(int s = j+1; s < H ; s++){
+                                if(arr[s][k] == '*'){
+                                    arr[s][k] = '.';
+                                    break;
+                                }
+
+                                if(arr[s][k] == '#'){
+                                    break;
+                                }
+                            }
+                        }
+
+                        else{
+                            for(int s = j-1; s >= 0; s--){
+                                if(arr[s][k] == '*'){
+                                    arr[s][k] = '.';
+                                    break;
+                                }
+
+                                if(arr[s][k] == '#'){
+                                    break;
+                                }
+                            }
+
+                        }
+                    }
+                }
+            }
+
+            //슈팅 끝------------------------------
+
+            // 이제 이동 -----------------------
+
+            else if(aw == 'U'){
+
+                L :for(int j = 0; j < H; j++) { // 이중반복문이여서 탈출 조건 달아주기
+                    for (int k = 0; k < W; k++) {
+                        if(arr[j][k] == '<' || arr[j][k] == '>' || arr[j][k] == '^' || arr[j][k] == 'v'){
+                            arr[j][k] = '^';
+                            if(j -1 >= 0 && arr[j-1][k] =='.'){
+                                arr[j-1][k] = arr[j][k];
+                                arr[j][k] = '.';
+                                break L;  //이동 한 부분이 겹칠수도 있으므로
+                            }
+
+                        }
+
+                    }
+                }
+            }
+
+            else if(aw == 'D'){
+
+                L :for(int j = 0; j < H; j++) { // 이중반복문이여서 탈출 조건 달아주기
+                    for (int k = 0; k < W; k++) {
+                        if(arr[j][k] == '<' || arr[j][k] == '>' || arr[j][k] == '^' || arr[j][k] == 'v'){
+                            arr[j][k] = 'v';
+                            if(j +1 < H && arr[j+1][k] =='.'){
+                                arr[j+1][k] = arr[j][k];
+                                arr[j][k] = '.';
+
+                                break L;
+                            }
+
+                        }
+
+                    }
+                }
+            }
+
+            else if(aw == 'L'){
+
+                L :for(int j = 0; j < H; j++) { // 이중반복문이여서 탈출 조건 달아주기
+                    for (int k = 0; k < W; k++) {
+                        if(arr[j][k] == '<' || arr[j][k] == '>' || arr[j][k] == '^' || arr[j][k] == 'v'){
+                            arr[j][k] = '<';
+                            if(k-1 >= 0 && arr[j][k-1] =='.'){
+                                arr[j][k-1] = arr[j][k];
+                                arr[j][k] = '.';
+
+                                break L;
+                            }
+
+                        }
+
+                    }
+                }
+            }
+
+            else{
+
+                L :for(int j = 0; j < H; j++) { // 이중반복문이여서 탈출 조건 달아주기
+                    for (int k = 0; k < W; k++) {
+                        if(arr[j][k] == '<' || arr[j][k] == '>' || arr[j][k] == '^' || arr[j][k] == 'v'){
+                            arr[j][k] = '>';
+                            if(k+1 < W && arr[j][k+1] =='.'){
+                                arr[j][k+1] = arr[j][k];
+                                arr[j][k] = '.';
+
+                                break L;
+                            }
+
+                        }
+
+                    }
                 }
 
             }
-            index ++;
+
+
+
+
+
+
 
         }
-
-        return answer;
     }
+
+
+
 }
+
+
